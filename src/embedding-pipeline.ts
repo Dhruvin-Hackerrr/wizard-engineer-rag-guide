c1a4545f7c3841a8546404663f5cb88fe77707d7
Dhruvin-Hackerrr <pdexperimental@gmail.com>
2026-07-11 20:32:43 +0530

    feat: initialize project with Express server and LangChain-based RAG embedding pipeline
    successfully build embedding pipeline by using pinecone as database and cohere as embedding model and tested using remote blog's url
    
    also implement chunk transfer using langchain's textsplitter

---
+++ src/embedding-pipeline.ts
@@ -0,0 +1,46 @@
+import { Document } from '@langchain/core/documents'
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { CohereEmbeddings } from "@langchain/cohere";
+import { PineconeStore } from '@langchain/pinecone';
+import { Pinecone as PineconeClient } from '@pinecone-database/pinecone'
+
+import { CheerioWebBaseLoader } from '@langchain/community/document_loaders/web/cheerio';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
+export async function docEmbedding(url: string) {
+    const loader = new CheerioWebBaseLoader(url)
+    const parseDocs = await loader.load()
+
+    const DocsWithMeta = parseDocs.map((doc) => {
+        doc.metadata.source = url;
+        return doc;
+    })
+
+    const docs = DocsWithMeta.flat();
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    })
+
+    const allSplits = await textSplitter.splitDocuments(docs);
+
+    const embeddings = new CohereEmbeddings({
+        model: "embed-english-v3.0",
+        apiKey: process.env.COHERE_API_KEY as string
+    })
+
+    const pinecone = new PineconeClient({
+        apiKey: process.env.PINECONE_API_KEY as string,
+    })
+    const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX as string)
+
+    const vectorStore = new PineconeStore(embeddings, { pineconeIndex, maxConcurrency: 5 })
+    await vectorStore.addDocuments(allSplits)
+
+    console.log("finished Embedding .... docs added", )    
+}
+
+await docEmbedding("https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/")
